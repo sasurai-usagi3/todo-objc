e1c8a17e72dc965c804839ac9dbe936eb3661f8b
Takumu Uyama <t_uyama@r.recruit.co.jp>
2020-04-02 11:38:42 +0900

    update: 配列からリスト生成

--- todo/Presentation/TaskListViewController.m
+++ todo/Presentation/TaskListViewController.m
@@ -1,7 +1,10 @@
 #import "TaskListViewController.h"
 #import "TaskItemView.h"
+#import "Task.h"
 
-@implementation TaskListViewController
+@implementation TaskListViewController {
+@private NSArray *tasks;
+}
 @synthesize table;
 
 - (void)viewDidLoad {
@@ -9,17 +12,26 @@
 
     table.delegate = self;
     table.dataSource = self;
+
+    // とりあえず仮で入れとく
+    Task *task1 = [Task title:@"test1" date:[NSDate dateWithTimeIntervalSince1970:24 * 60 * 1000]];
+    Task *task2 = [Task title:@"test2" date:[NSDate dateWithTimeIntervalSince1970:24 * 60 * 2000]];
+    Task *task3 = [Task title:@"test3" date:[NSDate dateWithTimeIntervalSince1970:24 * 60 * 3000]];
+
+    tasks = [NSArray arrayWithObjects:task1, task2, task3, nil];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
+    return [tasks count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     NSBundle *bundle = [NSBundle mainBundle];
-    UINib *nib2 = [UINib nibWithNibName:@"TaskItemView" bundle:bundle];
+    UINib *nib = [UINib nibWithNibName:@"TaskItemView" bundle:bundle];
+    TaskItemView *item = [nib instantiateWithOwner: self options: nil][0];
+    Task *task = tasks[indexPath.row];
 
-    TaskItemView *item = [nib2 instantiateWithOwner: self options: nil][0];
+    [item title:task.title date:task.date];
 
     return item;
 }
